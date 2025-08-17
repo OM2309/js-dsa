50261a0dd282d2a4e2dcf786ef4e3f0a10f379b7
Om <23anuragsharma@gmail.com>
2025-08-17 10:18:16 +0530

    woirk

---
+++ 852.js
@@ -0,0 +1,17 @@
+// var peakIndexInMountainArray = function (arr) {
+//   let peakValue = -Infinity;
+//   let peakIndex = -1;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > peakValue) {
+//       peakValue = arr[i];
+//       peakIndex = i;
+//     }
+//   }
+
+//   return peakIndex;
+// };
+
+// console.log(peakIndexInMountainArray([0, 1, 0])); // 1
+// console.log(peakIndexInMountainArray([0, 2, 1, 0])); // 1
+// console.log(peakIndexInMountainArray([0, 10, 5, 2])); // 1
